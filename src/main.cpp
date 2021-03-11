75b9f758a386133fa3899b010a73a449ae28c1a0
soospeter <soos.peter@yahoo.com>
2021-03-11 19:35:58 +0100

    Handle STL exceptions in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.setWindowTitle("Hello world!");
-    window.resize(320, 240);
-    window.show();
-    return app.exec();
+    try
+    {
+        QApplication app(argc, argv);
+        QWidget window;
+        window.setWindowTitle("Hello world!");
+        window.resize(320, 240);
+        window.show();
+        return app.exec();
+    }
+    catch(const std::exception& e)
+    {
+        qFatal(e.what());
+    }
 }
