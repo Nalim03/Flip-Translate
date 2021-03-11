34bc372670ce6a23658c4351859279025e600b82
soospeter <soos.peter@yahoo.com>
2021-03-11 20:13:00 +0100

    Structs for a few SQL record types

--- src/persistence/sql_persistence.cpp
+++ src/persistence/sql_persistence.cpp
@@ -4,10 +4,10 @@ SQLPersistence::SQLPersistence(const char* dbFilename)
     : db(dbFilename, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE)
 {
     db.exec("CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, language_id INTEGER, word TEXT)");
-    db.exec("CREATE TABLE IF NOT EXISTS synonyms (id INTEGER PRIMARY KEY, word_id INTEGER, synonym TEXT)");
+    db.exec("CREATE TABLE IF NOT EXISTS synonyms (id INTEGER PRIMARY KEY, word_id INTEGER, synonym_word_id INTEGER)");
 
     db.exec("CREATE TABLE IF NOT EXISTS decks (id INTEGER PRIMARY KEY, name TEXT)");
     db.exec("CREATE TABLE IF NOT EXISTS cards (id INTEGER PRIMARY KEY, deck_id INTEGER, source_word_id INTEGER, target_word_id INTEGER)");
 
-    db.exec("CREATE TABLE IF NOT EXISTS attempts (id INTEGER PRIMARY KEY, time INTEGER, card_id INTEGER, result INTEGER)");
+    db.exec("CREATE TABLE IF NOT EXISTS attempts (id INTEGER PRIMARY KEY, time UNSIGNED BIG INT, card_id INTEGER, result INT8)");
 }
--- src/persistence/sql_persistence.h
+++ src/persistence/sql_persistence.h
@@ -1,9 +1,43 @@
 #ifndef SQL_PERSISTENCE
 #define SQL_PERSISTENCE
 
+#include <QtGlobal>
+#include <QLocale>
+#include <QString>
+#include <SQLiteCpp/SQLiteCpp.h>
+
 #include "iflip_persistence.h"
 
-#include <SQLiteCpp/SQLiteCpp.h>
+struct WordRecordData
+{
+    qint32 id;
+    QLocale::Language language;
+    QString word;
+};
+
+struct CardRecordData
+{
+    qint32 id;
+    qint32 deckId;
+    qint32 sourceWordId;
+    qint32 targetWordId;
+};
+
+enum class AttemptResult : qint8
+{
+    CouldNotRecall,
+    Easy,
+    Moderate,
+    Difficult
+};
+
+struct AttemptRecordData
+{
+    qint32 id;
+    quint64 time;
+    qint32 cardId;
+    AttemptResult result;
+};
 
 class SQLPersistence : public IFlipPersistence
 {
